25a4710f4519e52121b6d45532daee20aba68aea
Eduardo Mancera <emancera_nieto2000@hotmail.com>
2025-02-26 20:23:07 -0600

    Cambio total en codigo

--- ingresos.py
+++ ingresos.py
@@ -1,2 +1,8 @@
-print("Hola amigo: " + input("Dame tu nombre wey:") + " tienes: " + input("Dame tu edad perro: ") + " años de no bañarte")
-print("Buenas tardes")
+nombre = input("Hola mi perro, como se llama tu nombre?")
+ventas = float(input(f"Hola {nombre}, cuanto has vendido en el mes?"))
+print(ventas)
+print("Deja calculo tus comisiones basadas en el 13% total")
+
+resultado = round(ventas * 13 / 100,2)
+
+print(f"El total de tus comisiones es: ${resultado}")
