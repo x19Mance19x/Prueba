707679ef01bafc63a8a4c513aa40abce5695659a
Eduardo Mancera <emancera_nieto2000@hotmail.com>
2025-02-26 19:15:12 -0600

    Nueva version

--- ingresos.py
+++ ingresos.py
@@ -1,1 +1,1 @@
-print("Hola perro: " + input("Dame tu nombre wey:") + " tienes: " + input("Dame tu edad perro: ") + " años")
+print("Hola amigo: " + input("Dame tu nombre wey:") + " tienes: " + input("Dame tu edad perro: ") + " años de no bañarte")
